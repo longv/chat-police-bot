0430555723a95515aa766b95d498b45c2611f7bd
Long Vu <longvh96@gmail.com>
2021-11-20 10:12:09 +0000

    Log bot to discord server

---
+++ index.js
@@ -0,0 +1,19 @@
+const { Client, Intents } = require('discord.js')
+const client = new Client({ intents: [
+  Intents.FLAGS.GUILDS, 
+  Intents.FLAGS.GUILD_MESSAGES,
+  Intents.FLAGS.DIRECT_MESSAGES
+]})
+
+client.on("ready", () => {
+  console.log(`logged in as ${client.user.tag}!`)
+})
+
+client.on("messageCreate", msg => {
+  console.log("Message coming")
+  if(msg.content === "ping"){
+    msg.reply("pong")
+  }
+})
+
+client.login(process.env['TOKEN'])
