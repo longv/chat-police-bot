9977ba6ab00578c113ea65749203ae149a9e7eb8
Mateusz Utracki <31111740+mu7trak@users.noreply.github.com>
2021-11-20 17:14:49 +0100

    Improve basic method. Start model based learning method

---
+++ src/intent/context_filter/context_filter.py
@@ -0,0 +1,76 @@
+import spacy
+import random
+import pathlib
+import en_core_web_sm
+import logging
+
+# Base setup to get context/intent
+# Level 1 get it working by seeing basic text from file
+# Level 2 no idea
+# Level 3 use language model with chatbot to get the context. I think I will go straight from 1 to 3
+path_base_data = pathlib.Path.cwd() / "context_filter" / "data"
+path_stoptalk_words = path_base_data / "en_stoptalk_words.txt"
+path_performance_words = path_base_data / "en_performance_words.txt"
+path_stoptalk_fixphrases = path_base_data / "en_stoptalk_fixphrases"
+
+
+class ContextFilter:
+    def __init__(
+            self,
+            analysis_type="",
+            nlp=None,
+            language="en",
+    ):
+        self.language = language
+        self.analysis_type = analysis_type
+        self.nlp = nlp
+
+    @staticmethod
+    def check_existance_from_wordsfile(filename, text_words):
+        """
+        Check if text words are present in filename list
+        """
+        is_from_wordsfile = None
+        with open(filename) as f:
+            single_words = [line.rstrip() for line in f]
+            logging.info(f"Filename words: {single_words}")
+            is_from_wordsfile = any(word in text_words for word in single_words)
+        return is_from_wordsfile
+
+    def get_intent_response(self, reason):
+        response = ''
+        if reason == "stop_talking":
+            with open(path_stoptalk_fixphrases) as f:
+                single_words = [line.rstrip() for line in f]
+                response = random.choice(single_words)
+        return response
+
+    def get_context_level1(self, text):
+        """
+        Very basic function to get an idea if the basic flow is working
+        """
+        intent = dict()
+        intent["reason"] = "good"
+        split_words = text.split()
+
+        is_stoptalk = self.check_existance_from_wordsfile(filename=path_stoptalk_words,
+                                                          text_words=split_words)
+        is_performance = self.check_existance_from_wordsfile(filename=path_performance_words,
+                                                             text_words=split_words)
+        if is_stoptalk:
+            intent["reason"] = "stop_talking"
+        if is_performance:
+            intent["reason"] = "bad_performance"
+        self.get_intent_response(reason=intent["reason"])
+
+    def get_context_level2(self, text):
+        pass
+
+    def get_context_level3(self, text):
+        if self.language == "en":
+            self.nlp = en_core_web_sm.load()
+        else:
+            raise NotImplementedError("This language is currently not implemented")
+        nlp_text = self.nlp(text)
+        print(nlp_text)
+        return nlp_text
--- src/intent/main.py
+++ src/intent/main.py
@@ -1,17 +1,20 @@
+
+import os
+import pathlib
 from sys import exit
 import argparse
 from context_filter.context_filter import ContextFilter
 
 
 def main():
-    parser = argparse.ArgumentParser(description='Context filter console utility')
+    parser = argparse.ArgumentParser(description="Context filter console utility")
     group = parser.add_mutually_exclusive_group()
-    group.add_argument('-t', '--text', dest='text', help='Test the given text for context')
-    group.add_argument('-f', '--file', dest='path', help='Test the given file for context')
+    group.add_argument("-t", "--text", dest="text", help="Test the given text for context")
+    group.add_argument("-f", "--file", dest="path", help="Test the given file for context")
     # parser.add_argument('-l', '--language', dest='language', default='en',
     #                     help='Test for context using specified languages (comma separated)')
-    parser.add_argument('-o', '--output', dest='output_file', help='Write the censored output to a file')
-    parser.add_argument('--show', action='store_true', help='Print the censored text')
+    parser.add_argument("-o", "--output", dest="output_file", help="Write the censored output to a file")
+    parser.add_argument("--show", action="store_true", help="Print the censored text")
 
     args = parser.parse_args()
 
@@ -23,16 +26,18 @@ def main():
         text = args.text
     elif args.path:
         with open(args.path) as f:
-            text = ''.join(f.readlines())
+            text = "".join(f.readlines())
     else:
-        text = ''
+        text = ""
 
     cf = ContextFilter()
+    check_text = cf.get_context_level1(text)
+
     check_text = cf.get_context_level3(text)
     print(f"Text check: {check_text}")
 
     if args.output_file:
-        with open(args.output_file, 'w') as f:
+        with open(args.output_file, "w") as f:
             f.write(check_text)
         print("Censored text written to output file at: " + args.output_file)
 
@@ -40,5 +45,5 @@ def main():
         return
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     main()
