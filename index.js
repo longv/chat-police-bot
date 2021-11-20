384a65988a09ad7ca17b492c6cc3d49ec8927400
Tran Cong Minh <minh.trancong@metropolia.fi>
2021-11-20 21:07:08 +0700

    test the toxicity filter

--- index.js
+++ index.js
@@ -5,9 +5,9 @@ const client = new Client({ intents: [
   Intents.FLAGS.DIRECT_MESSAGES
 ]})
 
-
-const MessageCollection =[]
-
+let messageCollection = []
+let i=0
+let badFilter=[]
 
 client.on("ready", () => {
   console.log(`logged in as ${client.user.tag}!`)
@@ -16,33 +16,39 @@ client.on("ready", () => {
 client.on("messageCreate", msg => {
   console.log("Message coming")
 
-const messageObject = {
+  const messageObject = {
     body: msg.content,
-    id: MessageCollection.length +1,
-    score: [Math.floor(Math.random()*1),Math.floor(Math.random()*1),
-      Math.floor(Math.random()*1),Math.floor(Math.random()*1),
-      Math.floor(Math.random()*1),Math.floor(Math.random()*1)],
-    toxicity: false
-      
-    }
+    id: messageCollection.length +1,
+    score: [
+      Math.floor(Math.random()*2),Math.floor(Math.random()*2),
+      Math.floor(Math.random()*2),Math.floor(Math.random()*2),
+      Math.floor(Math.random()*2),Math.floor(Math.random()*2)
+    ],
+    toxicity: false     
+  }
     
-    MessageCollection.concat(messageObject)
-    setMessagetoxicity(MessageCollection)
-  MessageCollection.map(message => {
+  console.log(JSON.stringify(messageObject))
+
+  messageCollection = messageCollection
+    .concat(messageObject)
+    .map(message => {
+      if (message.score.includes(1)) {
+        return { ...message, toxicity: true }
+      } else {
+        return message
+      }
+    })
+  badFilter = messageCollection.filter(message => message.toxicity == true)
+  console.log(JSON.stringify(badFilter))
+  console.log(badFilter.length)
+
+
+  console.log(JSON.stringify(messageCollection))
+
+  messageCollection.forEach(message => {
     console.log(message.body, message.id, message.score, message.toxicity)
+    
   })
 })
-
-const setMessagetoxicity = (mesSage) => {
-  mesSage.map(message => {
-    if (message.score.includes(1))
-    message.toxicity = true;
-  })
-}
-
-
-
-
-
 
 client.login(process.env['TOKEN'])
