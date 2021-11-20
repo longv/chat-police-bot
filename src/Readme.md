0a72419421cee343ecbcfb28cc1a5653d333352a
Mateusz Utracki <31111740+mu7trak@users.noreply.github.com>
2021-11-20 13:27:36 +0100

    Add some helping files

--- src/Readme.md
+++ src/Readme.md
@@ -7,3 +7,10 @@ https://drive.google.com/file/d/19jy3iuoWvuxDXbHTBgmsa2lzMeeQSRis/view?pli=1
 ### Model 1 - Toxicity classification
 
 ### Model 2 - Context classification
+Get context of given sentence and based on that setup response 
+
+Based on https://www.researchgate.net/figure/TOP15-toxic-words-in-the-LOL-chats-Numbers-include-both-EUNE-and-EUW-servers_tbl1_332776925
+
+I prepared en_performance_words.txt
+
+
---
+++ src/setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup
+
+PACKAGES = ['numpy==1.21.4', 'pandas==1.3.4', 'tensorflow==2.7.0']
+
+setup(
+    name='junction2021',
+    version='0.1.0',
+    packages=['numpy==1.21.4', 'pandas==1.3.4', 'tensorflow==2.7.0', 'profanity-filter=1.3.3'],
+    url='https://github.com/longv/chat-police-bot',
+    license='GPLv3',
+    author='mu7tr',
+    author_email='',
+    description='chat-police-bot'
+)
