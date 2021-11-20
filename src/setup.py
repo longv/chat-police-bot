ad0d4adbafc65ac8397f19b64f647b37bd61a007
Mateusz Utracki <31111740+mu7trak@users.noreply.github.com>
2021-11-20 17:14:59 +0100

    Improve basic method. Start model based learning method

--- src/setup.py
+++ src/setup.py
@@ -1,11 +1,11 @@
 from setuptools import setup
 
-PACKAGES = ['numpy==1.21.4', 'pandas==1.3.4', 'tensorflow==2.7.0']
+PACKAGES = ['numpy', 'pandas', 'tensorflow', 'rasa']
 
 setup(
     name='junction2021',
     version='0.1.0',
-    packages=['numpy==1.21.4', 'pandas==1.3.4', 'tensorflow==2.7.0', 'profanity-filter=1.3.3'],
+    packages=PACKAGES,
     url='https://github.com/longv/chat-police-bot',
     license='GPLv3',
     author='mu7tr',
